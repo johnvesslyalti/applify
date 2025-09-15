cc55f3e19c533e3b6e2ba45e75303c5c8e22c83c
johnvesslyalti <altijohnvessly@gmail.com>
2025-09-15 17:02:27 +0530

    Add layout structure and initialize footer and navbar components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ export default function RootLayout({
         className="dark"
         style={{colorScheme: "dark"}}
         >
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Job Application Tracker</title>
+            </head>
             <body>
                 <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
                     {children}
